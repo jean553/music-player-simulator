831ad89e5edc0d5020c9fec15ec9c558c61454a7
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2018-01-24 08:50:44 +0100

    add todo about string comparisons

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@ int main() {
 
     std::cout << "Imaginary player" << std::endl;
 
+    /* TODO: #1 comparisons of the input are made on std::string types,
+       and this is clearly heavy compared to an integer comparison */
+
     std::string input;
 
     while (input != "exit") {
