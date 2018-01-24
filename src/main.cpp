138f0c5bd70d8e964530bb0c8420a650c3e31e58
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2018-01-24 08:42:31 +0100

    integrate the initial main source file

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+
+    std::cout << "Imaginary player" << std::endl;
+
+    std::string input;
+
+    while (input != "exit") {
+        std::cin >> input;
+    }
+
+    return EXIT_SUCCESS;
+}
