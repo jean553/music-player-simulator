f3d9fd187c5bdc8dfe1062e5166931998348a3b1
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2018-01-27 17:16:35 +0100

    move the function that removes duplicates into the playlist module

--- includes/playlist.hpp
+++ includes/playlist.hpp
@@ -11,10 +11,20 @@
 #include <string>
 
 /**
- * @brief list all the tracks
+ * @brief lists all the tracks
  *
  * @param playlist the playlist to list
  */
 void listAllTracks(const std::vector<std::string>& playlist);
+
+/**
+ * @brief removes all the duplicates values from the playlist
+ *
+ * @param playlist the playlist to edit
+ *
+ * the passed playlist is not constant because the function
+ * removes items from it if they are duplicated
+ */
+void removeDuplicates(std::vector<std::string>& playlist);
 
 #endif
