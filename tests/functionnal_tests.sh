bd1e8e0e4450fe6b4d78ab630d20428a6cb551d5
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2018-01-29 19:56:06 +0100

    refactor functionnal tests into an execute_test function

--- tests/functionnal_tests.sh
+++ tests/functionnal_tests.sh
@@ -7,5 +7,11 @@ OUTPUTS_PATH=tests/outputs
 OUTPUT_FILE=output
 ASSERT_EQUAL_COMMAND=cmp
 
-$BINARY_PATH < $INPUTS_PATH/play_track_without_track > $OUTPUT_FILE && echo "" >> output
-$ASSERT_EQUAL_COMMAND $OUTPUT_FILE $OUTPUTS_PATH/play_track_without_track
+execute_test() {
+
+    # execute 'echo' at the end as the output file does not have a valid EOF
+    $BINARY_PATH < $INPUTS_PATH/$1 > $OUTPUT_FILE && echo "" >> output
+    $ASSERT_EQUAL_COMMAND $OUTPUT_FILE $OUTPUTS_PATH/$1
+}
+
+execute_test play_track_without_track
