43e7eaee387e861ba24e880826da64f5749ae98c
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2018-01-29 21:31:04 +0100

    integrate functionnal test that checks for play, pause, resume and repeat, at least that it does not throw any error

--- tests/functionnal_tests.sh
+++ tests/functionnal_tests.sh
@@ -20,3 +20,4 @@ execute_test add_tracks_and_remove_duplicates
 execute_test add_tracks_and_remove_tracks
 execute_test show_track
 execute_test add_tracks_and_next_and_previous
+execute_test play_pause_resume_repeat
